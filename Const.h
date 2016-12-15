45c7f98597881cb2d7d138f5ec79893273eb90bc
totoro97 <totoro97@outlook.com>
2016-12-15 18:12:10 +0800

    add

--- Const.h
+++ Const.h
@@ -12,11 +12,12 @@ const int ICALL = 8;
 const int IRET = 9;
 const int IPUSHL = 0xA;
 const int IPOPL = 0xB;
+const int IBUBBLE = 0xC;
 
 const int FNONE = 0;
 
 const int RESP = 4;
-const int RNONE = 8;
+const int RNONE = 0xF;
 
 const int ALUADD = 0;
 
